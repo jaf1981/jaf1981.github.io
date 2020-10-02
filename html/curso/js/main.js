22356aef206bd8717c6ed12134392f6bd6aa2e50
Javier <javito81@hotmail.com>
2020-10-01 22:01:15 -0300

    Mejoras bootstrap y nuevos links

--- html/curso/js/main.js
+++ html/curso/js/main.js
@@ -2,12 +2,14 @@ var clases = [
     {
         video : "https://www.youtube.com/embed/bJXkFF9MkIU",
         contenido : "Clase 1. En esta primera clase haremos una introduccíón y comentaremos de qué se trata ser un programador full stack, veremos el temario que vamos a recorrer a lo largo del curso.",
-        material: "https://docs.google.com/presentation/d/1EOeAkufZOIaCh_b3YV2sMbmASvHpnIGZ-NCuYT97UZ8/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1EOeAkufZOIaCh_b3YV2sMbmASvHpnIGZ-NCuYT97UZ8/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1oTjeLLrU_2ynd-deI8_k15vrc_0DpkUl/view?usp=sharing"
     },
     {
         video : "https://www.youtube.com/embed/DmYwmcz_oI4",
         contenido : "Clase 2. HTML - Introducción a HTML5, elementos y tag's básicos.",
-        material: "https://docs.google.com/presentation/d/1-uMgB7YloOzgm_VJVYffNraYtW7BLYfdJWU7Y25tSec/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1-uMgB7YloOzgm_VJVYffNraYtW7BLYfdJWU7Y25tSec/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1JAG4DtRk_-zc0_mBDYX_wEbPUCyKUQVX/view?usp=sharing"
     },
     {
         video : "https://www.youtube.com/embed/dh73a2W0YzE",
@@ -22,7 +24,8 @@ var clases = [
     {
         video : "https://www.youtube.com/embed/OgagNk6-aAQ",
         contenido : "Clase 5. CSS - Introducción a CSS, hojas de estilo en cascada.",
-        material: "https://docs.google.com/presentation/d/1hLju2rsj_4a4Qz5EVbmoDFjZeoqxDz6Q_4rD7VbMPuw/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1hLju2rsj_4a4Qz5EVbmoDFjZeoqxDz6Q_4rD7VbMPuw/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1yFqib70TipqymWcX44CO-P-JJcRNOzkJ/view?usp=sharing"
     },
     {
         video : "https://www.youtube.com/embed/aS8OykFF0X8",
@@ -37,7 +40,8 @@ var clases = [
     {
         video : "https://www.youtube.com/embed/ssrcN9CULio",
         contenido : "Clase 8. CSS - Medidas absolutas, relativas, posicionamiento, display, introducción a flex-box.",
-        material: "https://docs.google.com/presentation/d/1VrVz3hkISe3d0NBL6VvIKYZ6svGAerZ-g4NPMAaMyec/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1VrVz3hkISe3d0NBL6VvIKYZ6svGAerZ-g4NPMAaMyec/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1CXG1eGVJfgXagxUsDL8LZJs7ny2uwz4j/view?usp=sharing"
     },      
     {
         video : "https://www.youtube.com/embed/Y1PFVNWlOFE",
@@ -52,7 +56,8 @@ var clases = [
     {
         video : "https://www.youtube.com/embed/RxX25yNgnYo",
         contenido : "Clase 11. CSS - Introducción a Bootstrap - ¿qué es un framework?.",
-        material: "https://docs.google.com/presentation/d/1pzpSvzv4Z45ifeTFr7qEUtU8F0sYsL8pQnGTcZ-4MBA/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1pzpSvzv4Z45ifeTFr7qEUtU8F0sYsL8pQnGTcZ-4MBA/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1LKGnRZFgB6NFWjc1Cp4a2R-i1PS4bnJi/view?usp=sharing"
     },      
     {
         video : "https://www.youtube.com/embed/A_hTfxkqmG8",
@@ -62,12 +67,14 @@ var clases = [
     {
         video : "https://www.youtube.com/embed/tH7RC_hmMi4",
         contenido : "Clase 13. Javascript - Introducción a la programación en Javascript. Variables, Tipos de Datos.",
-        material: "https://docs.google.com/presentation/d/1HCKAh_R_YNB0kH9V497tieovLLl9cY6nYuXOa_Yw5dI/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1HCKAh_R_YNB0kH9V497tieovLLl9cY6nYuXOa_Yw5dI/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1Al9lt2u6e8OORmTqf1OyZgRYEyc4XcXT/view?usp=sharing"
     },      
     {
         video : "https://www.youtube.com/embed/-PSSue7BT8Y",
         contenido : "Clase 14. Javascript - Operadores y Estructuras de Control principales.",
-        material: "https://docs.google.com/presentation/d/1MIflkZamU2rZ1trw5E5IRAtc9_F7JsqxTo_er9nsVpQ/edit?usp=sharing"
+        material: "https://docs.google.com/presentation/d/1MIflkZamU2rZ1trw5E5IRAtc9_F7JsqxTo_er9nsVpQ/edit?usp=sharing",
+        apoyo: "https://drive.google.com/file/d/1UCBJuO0fmEZWD4ndPJDB_B5Yz24c5q7B/view?usp=sharing"
     },              
 
 ]; 
@@ -78,4 +85,10 @@ function elegir_clase2() {
     document.getElementById("detalle_clase").innerHTML = clases[data2 - 1].contenido;
     document.getElementById("link_material").href = clases[data2 - 1].material;
     document.getElementById("link_material").innerText = "Link al material de lectura. #Clase "+ data2 + ".";
+    if (clases[data2 - 1].hasOwnProperty('apoyo') == true) {
+        document.getElementById('link_biblio').href = clases[data2 - 1].apoyo;
+        document.getElementById('link_biblio').className = "d-block my-5";
+    } else {
+        document.getElementById('link_biblio').className = "d-none";
+    }
 }
